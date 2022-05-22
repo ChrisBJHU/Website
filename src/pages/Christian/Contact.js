27c4978bbeb9b4ce6174ee863e47a0820ff286fb
Jonathan Bakhit <Jonathanbakhit02@gmail.com>
2022-05-22 01:06:06 -0500

    tyeet

--- src/pages/Christian/Contact.js
+++ src/pages/Christian/Contact.js
@@ -4,24 +4,45 @@ import ContactForm from '../../components/contactForm.js';
 const Contact = () => {
 	const contactRef = useNav('Contact');
 
-    const pageStyle = {
-        height: "1000px",
-    };
-
     const background = {
       backgroundColor: '#222629',
       height: '100%',
       overflow: 'hidden',
+      paddingBottom: '25px',
+      paddingTop: '25px',
+  }
+
+  const textStyle = {
+      fontSize: "20px",
+      color: 'lightblue',
+      position:'relative',
+      textAlign: 'center',
   }
   
   return (
-    <section ref={contactRef} id='contactContainer' style = {{height: '100%', width: '100%'}}>
-      <div style = {background}>
-        <div>
+    <div>
+      <section ref={contactRef} id='contactContainer' style = {{height: '100%', width: '100%'}}>
+        <div style = {background}>
           <ContactForm></ContactForm>
-        </div>
+      </div>
+    </section>
+    <div style = {background}>
+      <p style = {textStyle}>
+        Want to talk to me directly? Send me a email! 
+      </p>
+      <p style = {textStyle}>
+        My email is: <a href="mailto: chrisbakhit@gmail.com" type="email" style = {{color: "lightblue"}}>
+        chrisbakhit@gmail.com
+        </a>
+        </p>
+        <p style = {textStyle}>
+        Johns Hopkins Undergraduate
+        </p>
+        <p style = {textStyle}>
+        Computer Science & Applied Mathematics & Statistics Major
+      </p>
     </div>
-  </section>
+  </div>
   );
 };
 
