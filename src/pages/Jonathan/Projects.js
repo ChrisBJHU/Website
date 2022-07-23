54ec526f9fb4dd09ab4396b9f688b8c8aa123ffc
syncre02 <jbakhit1@jhu.edu>
2022-07-23 12:24:27 -0500

    update some graphical settings!

--- src/pages/Jonathan/Projects.js
+++ src/pages/Jonathan/Projects.js
@@ -22,113 +22,127 @@ const Projects = () => {
     backgroundColor: "#223882",
   };
 
+  const generalTileStyle = {
+    justifyContent: "center",
+    alignItems: "center",
+  }
+  
+
   const tileStyle = {
     opacity: inViewport ? 1 : 0,
-    padding: "0px 8px",
+    padding: "8px",
     borderRadius: "5px",
     border: "1px solid none #222629",
   };
+
+  const listProjects = [
+    {
+      title: "Delineo Modeling Project",
+      desc: "The research project I've been fortunate to help research and develop. We aim to create a sandbox for future epidemiological research.",
+      link: "https://covidweb.isi.jhu.edu/",
+      clickable: true,
+    },
+    {
+      title: "AnyTown, USA",
+      desc: "This Unity Project was a team project to implement a 'sims' like simulation of COVID to support Delineo.",
+      link: "https://hub.jhu.edu/2021/08/12/anytown-usa-simulator/",
+      clickable: true,
+    },
+    {
+      title: "Saint Mary & Archangel Micheal of Houston Website",
+      desc: "A website for the St. Mary & Archangel Micheal Church, designed and developed on React using Firebase as the backend.",
+      link: "https://www.saintmaryhouston.org/",
+      clickable: true,
+    },
+    {
+      title: "Southern Coptic Project",
+      desc: "A Flutter project to allow students to be able to see pdfs of lesson plans on their phones.",
+      link: "/",
+      clickable: false,
+    },
+    {
+      title: "Gemini Discord Bot",
+      desc: "A discord bot implemented in Python that serves as a private service to provide contest problems for students interested in Science Bowl.",
+      link: "https://github.com/ChrisBJHU/DiscordBot",
+      clickable: true,
+    },
+    {
+      title: "Gemini Discord Bot 2.0",
+      desc: "A discord bot that provides useful information to the user about watched stocks, website downtimes, and more.",
+      link: "/",
+      clickable: false,
+    },
+    {
+      title: "Covid Tracker (HopHacks 2020)",
+      desc: "A 48 hour hackathon project that aimed to give users information from the JHU dashboard using an API and providing a private tracing list. (Wordpress, Python)",
+      link: "https://github.com/ChrisBJHU/CovidTracker2020",
+      clickable: true,
+    },
+    {
+      title: "Jarvis Project",
+      desc: "A Raspberry Pi project to allow me to be able to control all of the smart home features in my house. I am to use this for automated security and recreation in the future.",
+      link: "/",
+      clickable: false,
+    },
+    {
+      title: "HopMC President and Head Developer",
+      desc: "Created and maintain a Minecraft Server deployment deploying a Virtual Machine using Microsoft Azure, and delivered constant updates to respond to data breaches such as the Log4j vulnerability.",
+      link: "/",
+      clickable: false,
+    },
+    {
+      title: "HopMC President and Head Developer",
+      desc: "Created and maintain a Minecraft Server deployment deploying a Virtual Machine using Microsoft Azure, and delivered constant updates to respond to data breaches such as the Log4j vulnerability.",
+      link: "/",
+      clickable: false,
+    },
+  ];
 
   useEffect(() => {
     const color = "#223882";
     document.body.style.backgroundColor = color;
   }, [inViewport]);
 
-  const projects = () => {
-    if(!isMobile) {
-      return (
-        <Grid container spacing={1} style={gridStyle}>
-          <Grid item style={tileStyle}>
-            <FadeIn delay={1500} transitionDuration={500} visible={inViewport}>
-              <Tile
-                link="https://hub.jhu.edu/2021/08/12/anytown-usa-simulator/"
-                title="AnyTown, USA"
-                text="The Unity Project that was created with Christian Bakhit and I for the purposes of Delineo. With this simulation, we hope to better gardner interest into the workings of Delineo and give policymakers an interactive 'game' for which to gain critical information."
-              />
-              <Tile
-                link="https://covidweb.isi.jhu.edu/"
-                title="Delineo Modeling Project"
-                text="Delineo is where I currently work as a Researcher and Developer. At Delineo, I help create revolutionary new COVID simulations for law makers to use. Currently, I serve as Team Leader for the Fullstack team and Product Owner of the Unity Project."
-              />
-            </FadeIn>
-          </Grid>
-          <Grid item style={tileStyle}>
-            <FadeIn delay={1500} transitionDuration={500} visible={inViewport}>
-              <Tile
-                link="https://github.com/ChrisBJHU/DiscordBot"
-                title="Gemini Discord Bot"
-                text="A discord bot implemented in Python that serves as a private service to provide contest problems for students interested in Science Bowl."
-              />
-              <Tile
-                link="https://github.com/ChrisBJHU/CovidTracker2020"
-                title="Covid Tracker (HopHacks 2020)"
-                text="An Hackathon Project that was built with a team of 4 people in a limited time-frame. This project sought to give users information about COVID and allow them to have a private tracing list. Used WordPress and Python."
-              />
-            </FadeIn>
-          </Grid>
-          <Grid item style={tileStyle}>
-            <FadeIn delay={1500} transitionDuration={500} visible={inViewport}>
-              <Tile
-                title="Traffic Light Simulator"
-                text="A traffic light simulator that was built with Python (pyGame and Keras Modeling) to simulate traffic light behavior and improve Traffic Simulations."
-                clickable="false"
-              />
-              <Tile
-                title="Speech Recognition and Encryption"
-                titleSize="30px"
-                text="Using Python and Google Cloud Services, I developed a Speech Recognition program that took input through a microphone or audio file and then which encrypted the message to read back to the user. It has both Encryption and Decryption Capabilties."
-                clickable="false"
-              />
-            </FadeIn>
-          </Grid>
-        </Grid>
-      );
-    } else {
-      return (
-        <FadeIn delay={1500} transitionDuration={500} visible={inViewport}>
-          <Grid container spacing={1} style={gridStyle}>
-            <Grid item style={tileStyle}>
-                <Tile
-                  link="https://hub.jhu.edu/2021/08/12/anytown-usa-simulator/"
-                  title="AnyTown, USA"
-                  text="The Unity Project that was created with Christian Bakhit and I for the purposes of Delineo. With this simulation, we hope to better gardner interest into the workings of Delineo and give policymakers an interactive 'game' for which to gain critical information."
-                />
-                <Tile
-                  link="https://covidweb.isi.jhu.edu/"
-                  title="Delineo Modeling Project"
-                  text="Delineo is where I currently work as a Researcher and Developer. At Delineo, I help create revolutionary new COVID simulations for law makers to use. Currently, I serve as Team Leader for the Fullstack team and Product Owner of the Unity Project."
-                />
-            </Grid>
-            <Grid item style={tileStyle}>
-                <Tile
-                  link="https://github.com/ChrisBJHU/DiscordBot"
-                  title="Gemini Discord Bot"
-                  text="A discord bot implemented in Python that serves as a private service to provide contest problems for students interested in Science Bowl."
-                />
-                <Tile
-                  link="https://github.com/ChrisBJHU/CovidTracker2020"
-                  title="Covid Tracker (HopHacks 2020)"
-                  text="An Hackathon Project that was built with a team of 4 people in a limited time-frame. This project sought to give users information about COVID and allow them to have a private tracing list. Used WordPress and Python."
-                />
-            </Grid>
-            <Grid item style={tileStyle}>
-                <Tile
-                  title="Traffic Light Simulator"
-                  text="A traffic light simulator that was built with Python (pyGame and Keras Modeling) to simulate traffic light behavior and improve Traffic Simulations."
-                  clickable="false"
-                />
-                <Tile
-                  title="Speech Recognition and Encryption"
-                  titleSize="30px"
-                  text="Using Python and Google Cloud Services, I developed a Speech Recognition program that took input through a microphone or audio file and then which encrypted the message to read back to the user. It has both Encryption and Decryption Capabilties."
-                  clickable="false"
-                />
-            </Grid>
-          </Grid>
-        </FadeIn>
+  const FormRow = (props) => {
+    let rowVal = props.row * 3;
+    let row = listProjects.slice(rowVal, rowVal + 3 );
 
+    return ( 
+      <React.Fragment>
+        {row.map((project, index) => {
+          return (
+          <Grid item key={index} style={tileStyle}>
+            <Tile
+              title={project.title}
+              text={project.desc}
+              link={project.link}
+              clickable={project.clickable}
+            />
+          </Grid>
+          );  
+        }
+        )}
+      </React.Fragment>
+    )
+  }
+
+
+  const projects = () => {
+    let grid = [];
+    for(let i = 0; i < Math.ceil(listProjects.length / 3); i++) {
+      grid.push(
+        <Grid container key={i} style = {isMobile ? {marginLeft: '0px'}: generalTileStyle}>
+          <FormRow row={i} />
+      </Grid>
       );
     }
+    return (
+      <Grid container spacing={1} style = {gridStyle}>
+        <FadeIn delay={1500} transitionDuration={500} visible={inViewport}>
+          {grid}
+        </FadeIn>
+    </Grid>
+    );
   };
 
   return (
