11caf42285dc2714697473aacda7f0e7e6da58ff
Christian Bakhit <chrisbakhit@gmail.com>
2023-02-01 19:21:43 -0500

    new website

--- src/nav/navLinks.js
+++
@@ -1,5 +0,0 @@
-export const navLinks = [
-  { navLinkId: "About", scrollToId: "aboutContainer" },
-  { navLinkId: "Projects", scrollToId: "projectContainer" },
-  { navLinkId: "Contact", scrollToId: "contactContainer" },
-];
